be8ddb5a103ccdca9baa30be9d9b41a847ad5f7c
sampaniony <sampanionyra55@gmail.com>
2025-06-24 11:35:40 +0200

    feat: change

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start max-w-2xl">
         <h1 className="text-3xl sm:text-4xl font-bold text-center sm:text-left">
-          Je suis Sampaniony
+          Je suis Sampaniony RAKOTOARIVELO
         </h1>
 
         <p className="text-base leading-relaxed text-center sm:text-left text-gray-700 dark:text-gray-300">
@@ -34,14 +34,7 @@ export default function Home() {
           </p>
         </section>
 
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <h1>INGETIS</h1>
       </main>
     </div>
   );
